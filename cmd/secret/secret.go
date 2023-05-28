f1a3fd50977bb7e4789f246c25ef0b209fcd2c3d
Sultan Sheikh <39882748+Encrypto07@users.noreply.github.com>
2023-05-28 14:44:12 +0530

    completed code

--- cmd/secret/secret.go
+++ cmd/secret/secret.go
@@ -1,1 +1,50 @@
 package secret
+
+import (
+	"errors"
+
+	"github.com/Encrypto07/star-secret-keeper/pkg/decryption"
+	"github.com/Encrypto07/star-secret-keeper/pkg/encryption"
+	"github.com/spf13/cobra"
+)
+
+var key string
+
+func Execute() error {
+	rootCmd := &cobra.Command{
+		Use:   "secret",
+		Short: "A CLI Secrets Manager Tool",
+	}
+
+	setCmd := &cobra.Command{
+		Use:   "set",
+		Short: "Set a secret",
+		RunE:  setSecret,
+	}
+	rootCmd.AddCommand(setCmd)
+
+	getCmd := &cobra.Command{
+		Use:   "get",
+		Short: "Get a secret",
+		RunE:  getSecret,
+	}
+	rootCmd.AddCommand(getCmd)
+
+	rootCmd.PersistentFlags().StringVar(&key, "key", "", "AES encryption key")
+
+	return rootCmd.Execute()
+}
+
+func setSecret(cmd *cobra.Command, args []string) error {
+	if len(args) != 1 {
+		return errors.New("invalid number of arguments")
+	}
+
+	value := args[0]
+
+	return encryption.SetSecret(value, key)
+}
+
+func getSecret(cmd *cobra.Command, args []string) error {
+	return decryption.GetSecret(key)
+}
--- main.go
+++ main.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+
+	"github.com/Encrypto07/star-secret-keeper/cmd/secret"
+)
 
 func main() {
-	fmt.Println("Hello world")
+	if err := secret.Execute; err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
 }
